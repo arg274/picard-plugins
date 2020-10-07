c6577e04c43c975f77d53b11d1e9d68de21db7c0
arg274 <deeparghya.csedu@gmail.com>
2020-10-07 18:10:49 +0600

    [Update] VGMdb Variables: Minor improvements

--- plugins/vgmdb_variables/vgmdb_variables.py
+++ plugins/vgmdb_variables/vgmdb_variables.py
@@ -1,4 +1,5 @@
 import re
+import unicodedata
 from functools import partial
 
 from picard.util import LockableObject
@@ -11,10 +12,11 @@ PLUGIN_NAME = 'VGMdb Variables'
 PLUGIN_AUTHOR = 'snobdiggy'
 PLUGIN_DESCRIPTION = 'Add additional variables fot VGMdb data. Only adds variables for products associated ' \
                      'with a release for the moment being.'
-PLUGIN_VERSION = '0.2'
+PLUGIN_VERSION = '0.2.1'
 PLUGIN_API_VERSIONS = ['2.0', '2.1', '2.2']
 
 
+# noinspection PyUnusedLocal
 class VGMdbMetadataProcessor(object):
 
     class VGMdbMetadataQueue(LockableObject):
@@ -70,7 +72,6 @@ class VGMdbMetadataProcessor(object):
 
         self.search_route = False
         self.search_route_iter_idx = 0
-        self.strict_mode = False
 
         self.albumpage_cache = {}
         self.albumpage_queue = self.VGMdbMetadataQueue()
@@ -100,7 +101,6 @@ class VGMdbMetadataProcessor(object):
     def search_mode_reset(self):
         self.search_route = False
         self.search_route_iter_idx = 0
-        self.strict_mode = False
 
     def check_album_request_cache(self, tagger, metadata, release_dict, vgmdb_id):
 
@@ -121,10 +121,17 @@ class VGMdbMetadataProcessor(object):
                     # noinspection PyProtectedMember
                     self.make_vgmdb_request(tagger, tagger._new_tracks[-1], release_dict, vgmdb_id)
                 else:
-                    # noinspection PyProtectedMember
-                    self.make_vgmdb_request(tagger, tagger._new_tracks[self.search_route_iter_idx],
-                                            release_dict, vgmdb_id)
-                    self.search_mode_increment()
+                    try:
+                        # noinspection PyProtectedMember
+                        self.make_vgmdb_request(tagger, tagger._new_tracks[self.search_route_iter_idx],
+                                                release_dict, vgmdb_id)
+                    except IndexError:
+                        self.search_mode_reset()
+                        # noinspection PyProtectedMember
+                        self.make_vgmdb_request(tagger, tagger._new_tracks[self.search_route_iter_idx],
+                                                release_dict, vgmdb_id)
+                    finally:
+                        self.search_mode_increment()
 
     def vgmdb_branching_controller(self, tagger, metadata, track_dict, release_dict):
 
@@ -156,17 +163,22 @@ class VGMdbMetadataProcessor(object):
     # noinspection PyMethodMayBeStatic
     def get_vgmdb_products(self, release_dict, response, strict_mode):
 
+        def sanitise(string):
+            pattern = re.compile(r'\W')
+            return unicodedata.normalize('NFKC', re.sub(pattern, '', string))
+
         def pack_products(_response):
 
             product_list_en = []
             product_list_jp = []
 
             try:
-                products = response['products']
-                for product in products:
-                    product_list_en.append(product['names']['en'])
-                    if 'ja' in product['names']:
-                        product_list_jp.append(product['names']['ja'])
+                if 'products' in response:
+                    products = response['products']
+                    for product in products:
+                        product_list_en.append(product['names']['en'])
+                        if 'ja' in product['names']:
+                            product_list_jp.append(product['names']['ja'])
 
                 return product_list_en, product_list_jp
 
@@ -188,7 +200,7 @@ class VGMdbMetadataProcessor(object):
             if 'artist' in artist:
                 source_artists.append(artist['artist']['name'].lower())
 
-        for personnel_type in ['performers', 'composers']:
+        for personnel_type in ['performers', 'composers', 'organizations']:
             if personnel_type in response:
                 for personnel in response[personnel_type]:
                     for name in personnel['names']:
@@ -196,10 +208,8 @@ class VGMdbMetadataProcessor(object):
 
         if 'Various Artists' in source_artists:
 
-            pattern = re.compile(r'\W')
-
-            source_title = re.sub(pattern, '', release_dict['title'])
-            vgmdb_titles = [re.sub(pattern, '', title.split(' / ', 1)[0]) for title in response['names']]
+            source_title = sanitise(release_dict['title'])
+            vgmdb_titles = [sanitise(title.split(' / ')[0]) for title in response['names']]
 
             for vgmdb_title in vgmdb_titles:
                 if vgmdb_title == source_title:
@@ -229,13 +239,12 @@ class VGMdbMetadataProcessor(object):
                     for title in album['titles']:
                         # Their data separation is really mediocre; this might have unintended
                         # results if the album itself has a slash
-                        if ' / ' in title:
-                            title = title.split(' / ', 1)[0]
+                        title = title.split(' / ', 1)[0]
                         if title.lower() == source_title:
                             vgmdb_id = re.sub(r'album/', '', album['link'])
                             vgmdb_id = vgmdb_id if vgmdb_id != '' else None
                             return vgmdb_id
-                # Just return the first album if no exact match is found
+                # Just return the first album if no exact match is found, will get filtered later anyway
                 vgmdb_id = re.sub(r'album/', '', albums[0]['link'])
                 vgmdb_id = vgmdb_id if vgmdb_id != '' else None
                 return vgmdb_id
@@ -268,7 +277,7 @@ class VGMdbMetadataProcessor(object):
             if en_products:
                 track_metadata['~vgmdb_products_en'] = en_products
             else:
-                log.warning('%s: English product title not found', PLUGIN_NAME)
+                log.debug('%s: English product title not found', PLUGIN_NAME)
             if jp_products:
                 track_metadata['~vgmdb_products_jp'] = jp_products
             else:
